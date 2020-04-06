56e631cc20325cbaba2bef2f9e4fadd334936f99
ikaninja <humhumhemhem@gmail.com>
2020-04-06 16:11:48 +0900

    コミット7

--- button/button.js
+++ button/button.js
@@ -16,6 +16,8 @@ var list = [];
 var result;
 var resultlist = [];
 var risoulist = [6,9,12,15,18,21,24,27,30,33];
+var point = 0;
+var text;
 
 //var music30s = new Audio();
 //music30s.src = 'music30s.mp3';
@@ -37,16 +39,38 @@ agoButton.onclick = () =>{
 }
 
 showButton.onclick = () =>{
-    if(list.length<11){
+    if(list.length<10){
         return;
     } else {
         for(var i = 0; i < 10; i++){
-            resultlist.push = risoulist[i] - list[i];
+            resultlist[i] = Math.abs(risoulist[i] - list[i]);
+        }
+
+        for(var j = 0; j < 10; j++){
+            if(resultlist[j] < 0.3){
+                point = point + 3;
+            } else if(resultlist[i] < 0.5) {
+                point = point + 2;
+            } else if(resultlist[i] < 0.8) {
+                point = point + 1;
+            } else {
+                point = point + 0;
+            }
+        }
+
+        if(point > 26){
+            result = '天才';
+        } else if(point > 20){
+            result = '才能あり';
+        } else if(point > 10){
+            result = '普通';
+        } else {
+            result = '才能なし';
         }
 
         const header = document.createElement('h1');
-        result = 'すばらしいよ'
-        header.innerText = "結果" + result;
+        text = "30点満点中　" + point + '点  ' + result;
+        header.innerText = text;
         resultArea.appendChild(header); 
 
         const link = document.createElement('a');
@@ -60,6 +84,24 @@ showButton.onclick = () =>{
         boatlink.target = '_blank'
         boatlink.innerText = "京都大学ボート部のホームページはこちら";
         linkArea.appendChild(boatlink); 
+
+        const anchor = document.createElement('a');
+        const hrefValue = 'https://twitter.com/intent/tweet?button_hashtag='
+          + encodeURIComponent('京大ボート部筋トレリズムゲーム')
+          + '&ref_src=twsrc%5Etfw';
+        anchor.setAttribute('href', hrefValue);
+        anchor.className = 'twitter-hashtag-button';
+        anchor.setAttribute('data-text', text);
+        anchor.innerText = 'Tweet #京大ボート部筋トレリズムゲーム';
+        tweetArea.appendChild(anchor);
+    
+        
+        // widgets.js の設定
+        const script = document.createElement('script');
+        script.setAttribute('src', 'https://platform.twitter.com/widgets.js');
+        tweetArea.appendChild(script);
+
+        showButton.remove();
     }
 }
 
