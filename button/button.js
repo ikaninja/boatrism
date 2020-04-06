e4715dcff1f6a419ad0e2f7f6a3d5ca8d134b758
ikaninja <humhumhemhem@gmail.com>
2020-04-06 14:22:41 +0900

    コミット5

--- button/button.js
+++ button/button.js
@@ -9,31 +9,50 @@ const linkArea = document.getElementById('link-area');
 const tweetArea = document.getElementById('tweet-area');
 
 
+var time0;
+var time1;
+var seconds;
+var list = [];
+var result;
+
 //var music30s = new Audio();
 //music30s.src = 'music30s.mp3';
 
-
-window.onload = function(){
-    var time0 = new Date();
+function timefunc(){
+    time0 = new Date();
     //music30s.play();
 }
 
+window.onload = timefunc;
+
 agoButton.onclick = () =>{
-    var time1 = new Date();
-    var seconds = (time1.getTime() - time0.getTime())/1000;
-    var list = [];
-    if(list.length < 11){
+    time1 = new Date();
+    seconds = (time1.getTime() - time0.getTime())/1000;
+    if(list.length < 10){
         list.push(seconds);
     }
 }
 
-resultArea.onclick = () =>{
-    if(list.length<11){
+showButton.onclick = () =>{
+    if(list.length<10){
         return;
     } else {
         const header = document.createElement('h1');
-        header.innerText = '結果';
-        resultArea.appendChild(header);     
+        result = 'すばらしい'
+        header.innerText = "結果" + result;
+        resultArea.appendChild(header); 
+
+        const link = document.createElement('a');
+        link.href = "https://ikaninja.github.io/boatrism/title/title.html";
+        link.target = '_self';
+        link.innerText = "もう一度挑戦する";
+        restartArea.appendChild(link);  
+
+        const boatlink = document.createElement('a');
+        boatlink.href = 'http://kyoto-univ-rowing.com/'
+        boatlink.target = '_blank'
+        boatlink.innerText = "京都大学ボート部のホームページはこちら";
+        linkArea.appendChild(boatlink);  
     }
 }
 
